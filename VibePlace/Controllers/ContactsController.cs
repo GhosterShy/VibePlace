99f10e6431e2570ee36168753c2d90949e16fb14
twothousandfive <crimsonclaws957@gmail.com>
2025-04-23 19:39:36 +0500

    Contacts

---
+++ VibePlace/Controllers/ContactsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VibePlace.Controllers
+{
+    public class ContactsController : Controller
+    {
+        public IActionResult Contacts()
+        {
+            return View();
+        }
+    }
+}
